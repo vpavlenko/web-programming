bf5f0265bd2112f71316a223b9f76181e3d20bc2
Vitaly Pavlenko <cxielamiko@gmail.com>
2014-11-19 14:47:56 +0300

    Flask demo: #03, moved html to templates

--- 04-http/blog/blog.py
+++ 04-http/blog/blog.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
 
 from data import entries
@@ -6,25 +6,13 @@ from data import entries
 
 @app.route("/")
 def home():
-    html = ''
-    for i, entry in enumerate(entries):
-        html += '<a href="post/{0}"><h2>{1}</h2></a><p>{2}</p>'.format(
-            i,
-            entry['title'],
-            entry['abstract']
-        )
-    return html
+    return render_template('home.html', entries=entries)
 
 
 @app.route("/post/<post_id>")
 def post(post_id):
-    entry = entries[int(post_id)]
-    html = '''
-<h2>{0}</h2>
-<h4>{1}</h4>
-{2}
-'''.format(entry['title'], entry['abstract'], entry['content'])
-    return html
+    entry = entries[int(post_id) - 1]
+    return render_template('post.html', entry=entry)
 
 
 if __name__ == "__main__":
