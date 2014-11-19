d8657aca994b0b5f22cfa6b7a3049cc422cc9e24
Vitaly Pavlenko <cxielamiko@gmail.com>
2014-11-19 13:22:18 +0300

    Flask demo: #02, added some real data into data.py module

--- 04-http/blog/blog.py
+++ 04-http/blog/blog.py
@@ -1,18 +1,30 @@
 from flask import Flask
 app = Flask(__name__)
 
+from data import entries
+
 
 @app.route("/")
 def home():
-    return """
-<a href="post/1">Post #1</a>
-<a href="post/2">Post #2</a>
-"""
+    html = ''
+    for i, entry in enumerate(entries):
+        html += '<a href="post/{0}"><h2>{1}</h2></a><p>{2}</p>'.format(
+            i,
+            entry['title'],
+            entry['abstract']
+        )
+    return html
 
 
 @app.route("/post/<post_id>")
 def post(post_id):
-    return "Concrete post #" + post_id
+    entry = entries[int(post_id)]
+    html = '''
+<h2>{0}</h2>
+<h4>{1}</h4>
+{2}
+'''.format(entry['title'], entry['abstract'], entry['content'])
+    return html
 
 
 if __name__ == "__main__":
