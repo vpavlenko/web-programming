5ac2b2e6a5753cbda9b15187aa27ec7720c1e3c0
Vitaly Pavlenko <cxielamiko@gmail.com>
2015-02-27 20:32:15 +0300

    Merge commit

--- 08-django-2/testingplatform/testingplatform/settings.py
+++ 08-django-2/testingplatform/testingplatform/settings.py
@@ -82,3 +82,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.7/howto/static-files/
 
 STATIC_URL = '/static/'
+
+LOGIN_REDIRECT_URL = '/'
--- 08-django-2/testingplatform/testingplatform/urls.py
+++ 08-django-2/testingplatform/testingplatform/urls.py
@@ -4,4 +4,11 @@ from django.contrib import admin
 urlpatterns = patterns('',
     url(r'^', include('lessons.urls')),
     url(r'^admin/', include(admin.site.urls)),
+
+    url(r'^login/$', 'django.contrib.auth.views.login', {
+        'template_name': 'login.html'
+    }, name='login'),
+    url(r'^logout/$', 'django.contrib.auth.views.logout', {
+        'next_page': 'index'
+    }, name='logout'),
 )
