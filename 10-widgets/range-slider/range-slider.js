0b90b5ad0251a82d725b703fc3e3ab10de291e02
Vitaly Pavlenko <cxielamiko@gmail.com>
2015-03-09 11:23:07 +0300

    Slider shows captions

--- 10-widgets/range-slider/range-slider.js
+++ 10-widgets/range-slider/range-slider.js
@@ -1,42 +1,68 @@
 (function() {
     "use strict";
 
-    function makeDraggable($handle, $base) {
-        $handle.draggable({
-            containment: $base,
-            scroll: false,
-            axis: "x"
-        });
-    }
-
     function map(x, in_min, in_max, out_min, out_max) {
         return (x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
     }
 
+    function makeDraggable(slider, $handle) {
+        $handle.draggable({
+            containment: slider.$base,
+            scroll: false,
+            axis: "x"
+        });
+        $handle.on('drag', function() {
+            slider.setValueFromPosition();
+        });
+    }
+
+
     function RangeSlider(selector, params) {
         this.$element = $(selector);
         this.$base = $('<div class="range-slider-base">');
-        this.$leftHandle = $('<div class="range-slider-handle range-slider-handle-left">L');
-        this.$rightHandle = $('<div class="range-slider-handle range-slider-handle-right">R');
+        this.$leftHandle = $('<div class="range-slider-handle-left">');
+        this.$leftCaption = $('<span class="range-slider-caption">').text('L');
+        this.$rightHandle = $('<div class="range-slider-handle-right">');
+        this.$rightCaption = $('<span class="range-slider-caption">').text('R');
 
-        this.$base.append(this.$leftHandle).append(this.$rightHandle);
+        this.$base.append(this.$leftHandle.append(this.$leftCaption))
+                  .append(this.$rightHandle.append(this.$rightCaption));
         this.$element.append(this.$base);
 
-        makeDraggable(this.$leftHandle, this.$base);
-        makeDraggable(this.$rightHandle, this.$base);
+        makeDraggable(this, this.$leftHandle);
+        makeDraggable(this, this.$rightHandle);
 
+        this.rangeWidth = this.$base.width() - this.$leftHandle.width();
         this.range = params.range;
         this.value(params.start);
     }
 
     RangeSlider.prototype.value = function(value) {
         if (value) {
-            this.$leftHandle.css('left', map(value[0], this.range[0], this.range[1], 0, this.$base.width()));
-            this.$rightHandle.css('right', map(value[1], this.range[0], this.range[1], this.$base.width(), 0));
+            this.$leftHandle.css('left',
+                map(value[0], this.range[0], this.range[1], 0, this.rangeWidth));
+            this.$rightHandle.css('right',
+                map(value[1], this.range[0], this.range[1], this.rangeWidth, 0));
+            this.showValueToCaptions();
             return this;
         } else {
             return this.value;
         }
+    };
+
+    RangeSlider.prototype.setValueFromPosition = function() {
+        this.showValueToCaptions();
+    };
+
+    RangeSlider.prototype.showValueToCaptions = function() {
+        var self = this;
+
+        function to_range(x) {
+            return Math.round(map(x, 0, self.rangeWidth, self.range[0], self.range[1]));
+        }
+
+        this.$leftCaption.text(to_range(this.$leftHandle.position().left));
+        this.$rightCaption.text(to_range(this.$rightHandle.position().left));
     };
 
     window.RangeSlider = RangeSlider;
